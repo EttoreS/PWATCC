7b0e933267b3a591ee97ca5f4a117f950a5a916a
EttoreS <ettore_etr@hotmail.com>
2018-12-12 23:54:04 -0200

    finish busca binaria

--- src/Components/binario.jsx
+++ src/Components/binario.jsx
@@ -6,7 +6,9 @@ class Binary extends React.Component {
         super(props)
         
         this.state = {
-            item: ''
+            item: '',
+            time: null, 
+            msg: ''
         }
     }
 
@@ -17,7 +19,7 @@ class Binary extends React.Component {
         console.log(this.props.array)
         
         
-        binarySearch(this.props.array, this.state.item)
+        this.triggerFunc()
     }
 
     handleChange = (event) => {
@@ -40,31 +42,48 @@ class Binary extends React.Component {
                     <input type="number" value={this.state.item} onChange={this.handleChange}/>
                     <button>Procurar</button>
                 </form>
+                <p>Result time Binary {this.state.time}</p>
+                <p>{this.state.msg}</p>
             </div>
         )
+    }
+    binarySearch(arr, i) {
+        console.log('array',arr)
+            var mid = Math.floor(arr.length / 2);
+            console.log(arr[mid], i);
+            
+            if (arr[mid] == i) {
+                console.log('match', arr[mid], i);
+                this.setState({
+                    msg: `encontrou  ${i}`
+                })
+                return arr[mid];
+            } else if (arr[mid] < i && arr.length > 1) {
+                console.log('mid lower', arr[mid], i);
+                return this.binarySearch(arr.splice(mid, Number.MAX_VALUE), i);
+            } else if (arr[mid] > i && arr.length > 1) {
+                console.log('mid higher', arr[mid], i);
+                return this.binarySearch(arr.splice(0, mid), i);
+            } else {
+                console.log('not here', i);
+                this.setState({
+                    msg: `não encontrou  ${i}`
+                })
+                return -1;
+            }  
+    }
+    triggerFunc(){
+        let t0 = performance.now();
+
+        this.binarySearch(this.props.array, this.state.item)
+
+        let t1 = performance.now();
+        this.setState({
+            time: (t1 - t0)
+        })
     }
 }
 
 
-function binarySearch(arr, i) {
-    console.log('array',arr)
-        var mid = Math.floor(arr.length / 2);
-        console.log(arr[mid], i);
-        
-        if (arr[mid] == i) {
-            console.log('match', arr[mid], i);
-            return arr[mid];
-        } else if (arr[mid] < i && arr.length > 1) {
-            console.log('mid lower', arr[mid], i);
-            return binarySearch(arr.splice(mid, Number.MAX_VALUE), i);
-        } else if (arr[mid] > i && arr.length > 1) {
-            console.log('mid higher', arr[mid], i);
-            return binarySearch(arr.splice(0, mid), i);
-        } else {
-            console.log('not here', i);
-            return -1;
-        }
-        
-}
 
 export default Binary;
